8cd1f0ace8acd640473ff7653cddd24d784d2164
paulamgonzalez <11alvaro11alonso@gmail.com>
2022-12-02 13:48:29 +0000

    Cuarto commit

--- Capsula/Assets/Scripts/Contador.cs
+++ Capsula/Assets/Scripts/Contador.cs
@@ -9,16 +9,20 @@ public class Contador : MonoBehaviour
     [SerializeField]
     public TextMeshProUGUI textoTiempo;
 
-    public GameObject conjuntoCapsula;
+    public GameObject conjuntoCapsula, conjuntoHud, capsulaIndividual;
     public Slider barraTiempo;
+    Vector3 escalaCapsula;
     float tiempoPartida;
     float tiempoMaximo = 30f;
     float tiempoMinimo = 10f;
     bool tiempoActivo = false;
+    bool capsulaDestruida = false;
 
     public void Start()
     {
-        tiempoPartida = Random.Range(tiempoMinimo, tiempoMaximo);
+        //tiempoPartida = Random.Range(tiempoMinimo, tiempoMaximo);
+        // Tiempo de 2 segundos para pruebas.
+        tiempoPartida = 2;
         barraTiempo.maxValue = tiempoPartida;
         tiempoActivo = true;
     }
@@ -26,6 +30,7 @@ public class Contador : MonoBehaviour
     public void Update()
     {
         float segundos = Mathf.FloorToInt(tiempoPartida % 60);
+        textoTiempo.text = segundos.ToString();
         if (tiempoActivo == true)
         {
             if (tiempoPartida > 0)
@@ -36,9 +41,20 @@ public class Contador : MonoBehaviour
             if (tiempoPartida <= 0)
             {
                 tiempoPartida = 0;
-                gameObject.SetActive(false);
+                Destroy(conjuntoHud);
+                LeanTween.scale(capsulaIndividual, escalaCapsula * 0.5f, 0.8f).setOnComplete(DestruirConjuntoCapsula);
             }
         }
-        textoTiempo.text = segundos.ToString();
+    }
+
+    public void DestruirConjuntoCapsula()
+    {
+        Object.Destroy(capsulaIndividual);
+        Component.Destroy(GetComponent<Contador>());
+        capsulaDestruida = true;
+        if (capsulaDestruida == true)
+        {
+            
+        }
     }
 }
